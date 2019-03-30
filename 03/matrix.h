8214c51f34d57dbd9837d1f4a67460023ea5e288
eles13 <48096405+eles13@users.noreply.github.com>
2019-03-31 02:18:18 +0300

    raw

---
+++ 03/matrix.h
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstdlib>
+#include <typeinfo>
+#include <exception>
+#include <stdexcept>
+#include <ios>
+
+using namespace std;
+
+
+
+class Matrix
+{
+    protected:
+    size_t rows;
+    size_t cols;
+    int** mat;
+    class ProxyClass
+    {
+        int* row;
+        size_t col;
+        public:
+        ProxyClass(int* r,size_t c): row(r),col(c) {}
+        int& operator[](int n)
+        {
+            if ((size_t)n>col)
+            throw out_of_range("");
+            return (row[n]); 
+        }
+    }; 
+    public:
+    Matrix(int r,int c): rows(r),cols(c) 
+    {
+        mat = (int**)calloc(rows,sizeof(int*));
+        for (size_t i=0; i<rows; i++)
+        mat[i]=(int*)calloc(cols,sizeof(int));
+    }
+    ~Matrix()
+    {
+        for (size_t i; i<rows; i++)
+        free(mat[i]);
+        free(mat);
+    }
+    const int getRows() {return rows;}
+    const int getColumns() {return cols;}
+    ProxyClass operator[](int n)
+    {
+        if ((size_t)n>=rows)
+        throw out_of_range("");
+        ProxyClass temp(mat[n],cols);
+        return temp;
+    }
+    void operator*=(const int& m2)
+    {
+        for(size_t i=0; i<rows; i++)
+            for (size_t j=0; j<cols; j++)
+            mat[i][j]*=m2;    
+    }
+    const bool operator==(Matrix& m1)
+    {
+        if ((m1.rows!=rows)||(m1.cols!=cols)) return false;
+        for (size_t i;i<m1.rows;i++)
+        for (size_t j;j<m1.cols;j++)
+        if (m1[i][j]!=mat[i][j]) return false;
+        return true;
+    }
+    const bool operator!=(Matrix& m1)
+    {
+        if ((m1.rows!=rows)||(m1.cols!=cols)) return true;
+        for (size_t i;i<m1.rows;i++)
+        for (size_t j;j<m1.cols;j++)
+        if (m1[i][j]!=mat[i][j]) return true;
+        return false;
+    }
+
+};
