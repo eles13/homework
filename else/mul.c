94a27425e313299908f71f9d5ef177c911e9ac80
eles13 <48096405+eles13@users.noreply.github.com>
2019-10-10 23:17:15 +0300

    done

--- else/mul.c
+++ else/mul.c
@@ -2,23 +2,41 @@
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
+#include <papi.h>
+#define NUM_EVENTS 1
 int main(int argc, char** argv)
 {
+    long long values[NUM_EVENTS];
+    int retval = PAPI_library_init(PAPI_VER_CURRENT);
+    assert(retval == PAPI_VER_CURRENT);
+    int events[NUM_EVENTS] = {
+        PAPI_L1_DCM,
+      };
     if (argc != 5)
         return -1;
     FILE* fina = fopen(argv[1],"rb+");
     FILE* finb = fopen(argv[2],  "rb+");
     FILE* fout = fopen(argv[3],"wb+");
+    FILE* fpapi = fopen("papis.txt", "a");
     if (!(fina&&finb&&fout))
       return -1;
     char ca,cb;
-    fread(&ca,sizeof(char),1,fina);
-    fread(&cb, sizeof(char),1,finb);
+    int check;
+    check = fread(&ca,sizeof(char),1,fina);
+    if (!check)
+      return -1;
+    check = fread(&cb, sizeof(char),1,finb);
+    if (!check)
+      return -1;
     if (ca!=cb)
         return -1;
     int32_t sizea,sizeb;
-    fread(&sizea,sizeof(int32_t),1,fina);
-    fread(&sizeb,sizeof(int32_t),1,finb);
+    check = fread(&sizea,sizeof(int32_t),1,fina);
+    if (!check)
+      return -1;
+    check = fread(&sizeb,sizeof(int32_t),1,finb);
+    if (!check)
+      return -1;
     if (sizea != sizeb)
         return -1;
     fwrite(&ca, sizeof(char),1,fout);
@@ -27,6 +45,7 @@ int main(int argc, char** argv)
     time_t start,end;
     FILE* timings = fopen("timings.txt","a");
     double passed;
+    sizea = sizeb = 100;
     if (ca == 'd')
     {
         int32_t** m32a = (int32_t**)calloc(sizeof(int32_t*),sizea);
@@ -43,14 +62,16 @@ int main(int argc, char** argv)
             assert(m32a[i]);
             assert(m32b[i]);
             assert(m32c[i]);
-            for (int32_t j = 0; j<sizea; j++)
-            {
-                fread(&m32a[i][j],sizeof(int32_t),1,fina);
-                fread(&m32b[i][j],sizeof(int32_t),1,finb);
-                m32c[i][j] = 0;
-            }
+            check = fread(&m32a[i],sizeof(int32_t),sizea,fina);
+            if (!check)
+              return -1;
+            check = fread(&m32b[i],sizeof(int32_t),sizea,finb);
+            if (!check)
+              return -1;
         }
         start = clock();
+        retval = PAPI_start_counters((int *)events, NUM_EVENTS);
+        assert(retval == 0);
         switch (mode)
         {
             case 0:
@@ -59,11 +80,12 @@ int main(int argc, char** argv)
                 {
                     for (int32_t j = 0; j<sizea; j++)
                     {
-                        m32c[i][j]=0;
+                        int32_t temp = 0;
                         for (int32_t k = 0; k<sizea; k++)
                         {
-                            m32c[i][j]+=m32a[i][k]*m32b[k][j];
+                            temp+=m32a[i][k]*m32b[k][j];
                         }
+                        m32c[i][j] = temp;
                     }
                 }
                 break;
@@ -137,12 +159,21 @@ int main(int argc, char** argv)
             }
         }
         end = clock();
+        retval = PAPI_read_counters(values, NUM_EVENTS);
+        assert(retval == 0);
+        fprintf(fpapi,"mode32: %d, L1 miss: %lf; L2 miss: %lf\n", mode, (double)values[0] , (double)values[1]);
+        retval = PAPI_stop_counters(values, NUM_EVENTS);
+        assert(retval == 0);
         passed = end - start;
         fprintf(timings,"%.30f mode32: %d\n",passed/CLOCKS_PER_SEC, mode);
         for (int32_t i = 0; i< sizea; i++)
         {
             for (int32_t j = 0; j<sizea; j++)
-                fwrite(&m32c[i][j],sizeof(int32_t),1,fout);
+            {
+                check = fwrite(&m32c[i][j],sizeof(int32_t),1,fout);
+                if (!check)
+                  return -1;
+            }
         }
         for (int32_t i = 0; i< sizea; i++)
         {
@@ -170,16 +201,16 @@ int main(int argc, char** argv)
             assert(m64a[i]);
             assert(m64b[i]);
             assert(m64c[i]);
-            for (int32_t j = 0; j<sizea; j++)
-            {
-                m64a[i][j] = rand()%1000;
-                m64b[i][j] = rand()%1000;
-                fread(&m64a[i][j],sizeof(int64_t),1,fina);
-                fread(&m64b[i][j],sizeof(int64_t),1,finb);
-                m64c[i][j] = 0;
-            }
+            check = fread(&m64a[i],sizeof(int64_t),sizea,fina);
+            if (!check)
+              return -1;
+            check = fread(&m64b[i],sizeof(int64_t),sizea,finb);
+            if (!check)
+              return -1;
         }
         start = clock();
+        retval = PAPI_start_counters((int *)events, NUM_EVENTS);
+        assert(retval == 0);
         switch (mode)
         {
             case 0:
@@ -188,11 +219,12 @@ int main(int argc, char** argv)
                 {
                     for (int32_t j = 0; j<sizea; j++)
                     {
-                        m64c[i][j]=0;
+                        int64_t temp = 0;
                         for (int32_t k = 0; k<sizea; k++)
                         {
-                            m64c[i][j]+=m64a[i][k]*m64b[k][j];
+                            temp+=m64a[i][k]*m64b[k][j];
                         }
+                        m64c[i][j] = temp;
                     }
                 }
                 break;
@@ -229,11 +261,12 @@ int main(int argc, char** argv)
                 {
                     for (int32_t i = 0; i<sizea; i++)
                     {
-                        m64c[i][j] = 0;
+                        int64_t temp = 0;
                         for (int32_t k = 0; k<sizea; k++)
                         {
-                            m64c[i][j]+=m64a[i][k]*m64b[k][j];
+                            temp+=m64a[i][k]*m64b[k][j];
                         }
+                        m64c[i][j] = temp;
                     }
                 }
                 break;
@@ -267,11 +300,24 @@ int main(int argc, char** argv)
         }
         end = clock();
         passed = end - start;
-        fprintf(timings,"%.30f mode64: %d\n",passed/CLOCKS_PER_SEC, mode);
+        retval = PAPI_read_counters(values, NUM_EVENTS);
+        assert(retval == 0);
+        check = fprintf(fpapi,"mode32: %d, L1 miss: %lf; L2 miss: %lf\n", mode, (double)values[0] , (double)values[1]);
+        if (!check)
+          return -1;
+        retval = PAPI_stop_counters(values, NUM_EVENTS);
+        assert(retval == 0);
+        check = fprintf(timings,"%.30f mode64: %d\n",passed/CLOCKS_PER_SEC, mode);
+        if (!check)
+          return -1;
         for (int32_t i = 0; i< sizea; i++)
         {
             for (int32_t j = 0; j<sizea; j++)
-                fwrite(&m64c[i][j],sizeof(int64_t),1,fout);
+            {
+                check = fwrite(&m64c[i][j],sizeof(int64_t),1,fout);
+                if (!check)
+                  return -1;
+            }
         }
         for (int32_t i = 0; i< sizea; i++)
         {
