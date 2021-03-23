a57cf254aab82de2c782aa613fb2df4e21add3a2
eles13 <piteles@yandex.ru>
2021-03-24 02:14:13 +0300

    debugged reflections

--- else/reflections/main.cpp
+++ else/reflections/main.cpp
@@ -6,7 +6,7 @@
 int rank, size;
 
 double formula_Mat(size_t i, size_t j){
-    return (double)i*2 + j*j;
+    return (double)i + j;
 }
 
 double* form_x(size_t n){
@@ -26,8 +26,10 @@ double* calc_x(size_t iter, const double* col, size_t n){
     }
     x[0] = col[iter] - sqrt(s + col[iter] * col[iter]);
     auto norm_x = sqrt(s + x[0] * x[0]);
-    for(size_t i = 0; i < n - iter; i++){
-        x[i] = x[i]/norm_x;
+    if (norm_x != 0) {
+        for (size_t i = 0; i < n - iter; i++) {
+            x[i] = x[i] / norm_x;
+        }
     }
     return x;
 }
@@ -40,32 +42,40 @@ void matByVec(const double* x, double* a, size_t n, size_t iter){
         rez[i]=0;
         for (size_t j = 0; j < n; j++)
         {
-            if(i<iter and j<iter){
+            if(i<iter or j<iter){
                 if(i==j){
                     elem = 1;
                 }else{
                     elem = 0;
                 }
             }else{
-                elem = -2*x[i]*x[j];
+                elem = -2*x[i-iter]*x[j-iter];
                 if(i==j){
                     elem += 1;
                 }
             }
-            rez[i] += a[i]*elem;
+            rez[i] += a[j]*elem;
         }
+    }
+    for(size_t i = 0; i < n; i++){
         a[i] = rez[i];
     }
+//    if(rank == 0){
+//        for(int j = 0; j < n; j++) {
+//            std::cout << a[j] << '\n';
+//        }
+//        std::cout<<"------------------\n";
+//    }
     free(rez);
 }
 
 double gen_b_row(size_t i, double** vec, size_t s){
-    auto sum = 0.0;
-    for(size_t idx = 0; i < s; i++) {
+    double sum = 0.0;
+    for(size_t idx = 0; idx < s; idx++) {
         sum += vec[idx][i];
     }
     double gsum;
-    MPI_Reduce(&sum, &gsum, size, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&sum, &gsum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     MPI_Bcast(&gsum, size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     return gsum;
 }
@@ -87,40 +97,46 @@ int main(int argc, char** argv) {
         return -1;
     }
     int mode = atoi(argv[2]);
-    size_t n = (size_t)pow(2.0,atof(argv[1]));
+    auto n = (size_t)pow(2.0,atoi(argv[1]));
     double* x_true;
     if(mode){
         x_true = form_x(n);
     }
     std::vector<int> my_cols;
-    size_t step = n % size;
     my_cols.reserve((int)(n / size));
     for(int i = 0; i < (int)(n / size); i++) {
-        my_cols.push_back(step + size * i);
+        my_cols.push_back(rank + size * i);
     }
     auto locmat = (double**) calloc(my_cols.size(), sizeof(double*));
+    auto copy_locmat = (double**) calloc(my_cols.size(), sizeof(double*));
     auto solution = (double*) calloc(my_cols.size(), sizeof(double));
-    for(int i = 0; i < my_cols.size(); i++){
+    for(size_t i = 0; i < my_cols.size(); i++){
         locmat[i] = (double*)calloc(n, sizeof(double));
+        copy_locmat[i] = (double*)calloc(n, sizeof(double));
         for(size_t j = 0; j < n; j++){
             locmat[i][j] = formula_Mat(my_cols[i],j);
+            copy_locmat[i][j] = locmat[i][j];
         }
     }
     auto b = (double*)calloc(n, sizeof (double));
     auto copy_b = (double*)calloc(n, sizeof (double));
     for(size_t j = 0; j < n; j++){
-        b[j] = gen_b_row(j, locmat, n);
+        b[j] = gen_b_row(j, locmat, my_cols.size());
         copy_b[j] = b[j];
     }
     MPI_Barrier(MPI_COMM_WORLD);
     double start = MPI_Wtime();
     for(size_t iter = 0; iter < n; iter++){
-        double* x = nullptr;
-        if(iter % size == rank){
+        double* x;
+        if((int)iter % size == rank){
             x = calc_x(iter, locmat[(int)(iter/size)], n);
         }
+        else{
+            x = (double*) calloc(n - iter, sizeof(double));
+        }
         MPI_Barrier(MPI_COMM_WORLD);
-        MPI_Bcast(x, n-iter, MPI_DOUBLE, iter%size, MPI_COMM_WORLD);
+        MPI_Bcast(x, n-iter, MPI_DOUBLE, (int)iter%size, MPI_COMM_WORLD);
+
         for(size_t i = 0; i < my_cols.size(); i++){
             matByVec(x, locmat[i], n, iter);
         }
@@ -131,15 +147,15 @@ int main(int argc, char** argv) {
     double t1 = MPI_Wtime() - start;
     start = MPI_Wtime();
     auto csum = (double *) calloc(n, sizeof(double));
-    for(size_t iter = n-1; iter >= 0; iter--){
-        if(iter%size == rank){
+    for(int iter = n-1; iter >= 0; iter--){
+        if((int)iter%size == rank){
             solution[int(iter/size)] = (b[iter] - csum[iter]) / locmat[int(iter/size)][iter];
-            for(size_t lociter = n-1; lociter >= iter; lociter--) {
+            for(size_t lociter = 0; lociter < iter; lociter++) {
                 csum[lociter] += solution[int(iter / size)] * locmat[int(iter / size)][lociter];
             }
         }
         MPI_Barrier(MPI_COMM_WORLD);
-        MPI_Bcast(&csum, n, MPI_DOUBLE, iter%size, MPI_COMM_WORLD);
+        MPI_Bcast(csum, n, MPI_DOUBLE, (int)iter%size, MPI_COMM_WORLD);
     }
     MPI_Barrier(MPI_COMM_WORLD);
     double t2 = MPI_Wtime() - start;
@@ -147,11 +163,11 @@ int main(int argc, char** argv) {
     auto newb  = (double*)calloc(n, sizeof(double));
     for(size_t i = 0; i < n; i++){
         for(size_t j = 0; j < my_cols.size(); j++) {
-            newb[i] += locmat[j][i] * solution[j];
+            newb[i] += copy_locmat[j][i] * solution[j];
         }
     }
     auto b_b  = (double*)calloc(n, sizeof(double));
-    double *x_x = nullptr;
+    auto x_x =  (double*)calloc(n, sizeof(double));
     MPI_Reduce(newb, b_b, n, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     if(mode){
         for(size_t i = 0; i < my_cols.size(); i++){
@@ -170,9 +186,9 @@ int main(int argc, char** argv) {
                 norm_x += x_x[i];
             }
             std::cout<<sqrt(norm_x)<<std::endl;
-            free(x_x);
         }
     }
+    free(x_x);
     free(copy_b);
     free(b_b);
     free(newb);
